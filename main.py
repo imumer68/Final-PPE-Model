fa2ae6df20419de5a075e12227c919511081f8da
Umer <95996580+imumer68@users.noreply.github.com>
2024-06-09 19:11:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,243 +1,66 @@
-
-from ultralytics import YOLO
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import numpy as np
 from PIL import Image
-import av
-import cv2
-from pytube import YouTube
 
-import settings
+import os
 
+import google.generativeai as genai
 
-def load_model(model_path):
-    """
-    Loads a YOLO object detection model from the specified model_path.
+from dotenv import load_dotenv, dotenv_values  # we can use load_dotenv or dotenv_values both perform the same task
 
-    Parameters:
-        model_path (str): The path to the YOLO model file.
+load_dotenv()
 
-    Returns:
-        A YOLO object detection model.
-    """
-    model = YOLO(model_path)
-    return model
+# print(os.getenv("MY_SECRET_KEY"))
 
+genai.configure(api_key=os.getenv("MY_SECRET_KEY")) 
 
-def display_tracker_options():
-    display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
-    is_display_tracker = True if display_tracker == 'Yes' else False
-    if is_display_tracker:
-        tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
-        return is_display_tracker, tracker_type
-    return is_display_tracker, None
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 1024,
+}
 
 
-def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
-    """
-    Display the detected objects on a video frame using the YOLOv8 model.
+def main():
+    st.title("Object Finder 🔍")
+    
+    disclaimer_message = """This is a object detector model so preferably use images containing different objects,tools... for best results 🙂"""
 
-    Args:
-    - conf (float): Confidence threshold for object detection.
-    - model (YoloV8): A YOLOv8 object detection model.
-    - st_frame (Streamlit object): A Streamlit object to display the detected video.
-    - image (numpy array): A numpy array representing the video frame.
-    - is_display_tracking (bool): A flag indicating whether to display object tracking (default=None).
+    # Hide the disclaimer initially
+    st.write("")
 
-    Returns:
-    None
-    """
+    # Show the disclaimer if the button is clicked
+    with st.expander("Disclaimer ⚠️", expanded=False):
+       st.markdown(disclaimer_message)
+    
 
-    # Resize the image to a standard size
-    image = cv2.resize(image, (720, int(720*(9/16))))
+    # Upload image through Streamlit
+    uploaded_image = st.file_uploader("Choose an image ...", type=["jpg", "jpeg", "png"])
 
-    # Display object tracking, if specified
-    if is_display_tracking:
-        res = model.track(image, conf=conf, persist=True, tracker=tracker)
-    else:
-        # Predict the objects in the image using the YOLOv8 model
-        res = model.predict(image, conf=conf)
+    if uploaded_image is not None:
+        # Display the uploaded image
+        st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
 
-    # # Plot the detected objects on the video frame
-    res_plotted = res[0].plot()
-    st_frame.image(res_plotted,
-                   caption='Detected Video',
-                   channels="BGR",
-                   use_column_width=True
-                   )
+        # Process the image (example: get image dimensions)
+        image = Image.open(uploaded_image)
+        width, height = image.size
+        st.write("Image Dimensions:", f"{width}x{height}")
 
+        if st.button("Identify the objects"):
 
-def play_youtube_video(conf, model):
-    """
-    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
+            st.success("Detecting...")
 
-    Parameters:
-        conf: Confidence of YOLOv8 model.
-        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+            vision_model = genai.GenerativeModel('gemini-pro-vision')
+            response = vision_model.generate_content(["Extract the objects in the provided image and output them in a list in alphabetical order",image])
 
-    Returns:
-        None
+            
+            st.write("The objects detected are \n", response.text)
 
-    Raises:
-        None
-    """
-    source_youtube = st.sidebar.text_input("YouTube Video url")
+            st.success("Thanks for visiting 🤩!!")
 
-    is_display_tracker, tracker = display_tracker_options()
-
-    if st.sidebar.button('Detect Objects'):
-        try:
-            yt = YouTube(source_youtube)
-            stream = yt.streams.filter(file_extension="mp4", res=720).first()
-            vid_cap = cv2.VideoCapture(stream.url)
-
-            st_frame = st.empty()
-            while (vid_cap.isOpened()):
-                success, image = vid_cap.read()
-                if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
-                else:
-                    vid_cap.release()
-                    break
-        except Exception as e:
-            st.sidebar.error("Error loading video: " + str(e))
+            st.info("For trying out with another image just click on browse files, enjoy 😄!!!")
 
 
-def play_rtsp_stream(conf, model):
-    """
-    Plays an rtsp stream. Detects Objects in real-time using the YOLOv8 object detection model.
-
-    Parameters:
-        conf: Confidence of YOLOv8 model.
-        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
-
-    Returns:
-        None
-
-    Raises:
-        None
-    """
-    source_rtsp = st.sidebar.text_input("rtsp stream url")
-    is_display_tracker, tracker = display_tracker_options()
-    if st.sidebar.button('Detect Objects'):
-        try:
-            vid_cap = cv2.VideoCapture(source_rtsp)
-            st_frame = st.empty()
-            while (vid_cap.isOpened()):
-                success, image = vid_cap.read()
-                if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
-                else:
-                    vid_cap.release()
-                    break
-        except Exception as e:
-            st.sidebar.error("Error loading RTSP stream: " + str(e))
-
-
-
-
-def play_webcam(conf, model):
-    """
-    Plays a webcam stream. Detects Objects in real-time using the YOLO object detection model.
-
-    Returns:
-        None
-
-    Raises:
-        None
-    """
-    st.sidebar.title("Webcam Object Detection")
-
-    webrtc_streamer(
-        key="example",
-        video_transformer_factory=lambda: MyVideoTransformer(conf, model),
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-        media_stream_constraints={"video": True, "audio": False},
-    )
-
-class MyVideoTransformer(VideoTransformerBase):
-    def __init__(self, conf, model):
-        self.conf = conf
-        self.model = model
-
-    def recv(self, frame):
-        image = frame.to_ndarray(format="bgr24")
-        processed_image = self._display_detected_frames(image)
-        st.image(processed_image, caption='Detected Video', channels="BGR", use_column_width=True)
-
-    def _display_detected_frames(self, image):
-        orig_h, orig_w = image.shape[0:2]
-        width = 720  # Set the desired width for processing
-
-        # cv2.resize used in a forked thread may cause memory leaks
-        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
-
-        if self.model is not None:
-            # Perform object detection using YOLO model
-            res = self.model.predict(input, conf=self.conf)
-
-            # Plot the detected objects on the video frame
-            res_plotted = res[0].plot()
-            return res_plotted
-
-        return input
-
-
-
-def play_stored_video(conf, model):
-    """
-    Plays a stored video file. Tracks and detects objects in real-time using the YOLOv8 object detection model.
-
-    Parameters:
-        conf: Confidence of YOLOv8 model.
-        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
-
-    Returns:
-        None
-
-    Raises:
-        None
-    """
-    source_vid = st.sidebar.selectbox(
-        "Choose a video...", settings.VIDEOS_DICT.keys())
-
-    is_display_tracker, tracker = display_tracker_options()
-
-    with open(settings.VIDEOS_DICT.get(source_vid), 'rb') as video_file:
-        video_bytes = video_file.read()
-    if video_bytes:
-        st.video(video_bytes)
-
-    if st.sidebar.button('Detect Video Objects'):
-        try:
-            vid_cap = cv2.VideoCapture(
-                str(settings.VIDEOS_DICT.get(source_vid)))
-            st_frame = st.empty()
-            while (vid_cap.isOpened()):
-                success, image = vid_cap.read()
-                if success:
-                    _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
-                else:
-                    vid_cap.release()
-                    break
-        except Exception as e:
-            st.sidebar.error("Error loading video: " + str(e))
+if __name__ == "__main__":
+    main()
