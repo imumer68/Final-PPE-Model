7d96eea5cbb7320dc53e4d5643630954739f00b1
Umer <95996580+imumer68@users.noreply.github.com>
2024-06-09 19:13:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,46 @@
-import streamlit as st
-from PIL import Image
 
-import os
-
-import google.generativeai as genai
-
-from dotenv import load_dotenv, dotenv_values  # we can use load_dotenv or dotenv_values both perform the same task
-
-load_dotenv()
-
-# print(os.getenv("MY_SECRET_KEY"))
-
-genai.configure(api_key=os.getenv("MY_SECRET_KEY")) 
-
-# Set up the model
-generation_config = {
-  "temperature": 0.9,
-  "top_p": 0.95,
-  "top_k": 40,
-  "max_output_tokens": 1024,
-}
-
-
-def main():
-    st.title("Object Finder 🔍")
-    
-    disclaimer_message = """This is a object detector model so preferably use images containing different objects,tools... for best results 🙂"""
-
-    # Hide the disclaimer initially
-    st.write("")
-
-    # Show the disclaimer if the button is clicked
-    with st.expander("Disclaimer ⚠️", expanded=False):
-       st.markdown(disclaimer_message)
-    
-
-    # Upload image through Streamlit
-    uploaded_image = st.file_uploader("Choose an image ...", type=["jpg", "jpeg", "png"])
-
-    if uploaded_image is not None:
-        # Display the uploaded image
-        st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
-
-        # Process the image (example: get image dimensions)
-        image = Image.open(uploaded_image)
-        width, height = image.size
-        st.write("Image Dimensions:", f"{width}x{height}")
-
-        if st.button("Identify the objects"):
-
-            st.success("Detecting...")
-
-            vision_model = genai.GenerativeModel('gemini-pro-vision')
-            response = vision_model.generate_content(["Extract the objects in the provided image and output them in a list in alphabetical order",image])
-
-            
-            st.write("The objects detected are \n", response.text)
-
-            st.success("Thanks for visiting 🤩!!")
-
-            st.info("For trying out with another image just click on browse files, enjoy 😄!!!")
-
-
-if __name__ == "__main__":
-    main()
+import cv2
+import numpy as np
+import av
+import mediapipe as mp
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+def process(image):
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+# Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+    return cv2.flip(image, 1)
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+img = process(img)
+return av.VideoFrame.from_ndarray(img, format="bgr24")
+webrtc_ctx = webrtc_streamer(
+    key="WYH",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
+    video_processor_factory=VideoProcessor,
+    async_processing=True,
+)
