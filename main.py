4b19f08e889c710220e9d28203e0b2c74daa5053
Umer <95996580+imumer68@users.noreply.github.com>
2024-06-09 19:06:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import cv2
-import streamlit as st
 
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
-
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    img = cv2.Canny(frame, 100, 200)  # Some image processing
+    cv2.imshow('frame', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
