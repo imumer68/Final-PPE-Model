6f24faa16db940edf16a6a6358ac2364c39f8b9d
Umer <95996580+imumer68@users.noreply.github.com>
2024-06-10 10:35:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,62 @@
 import cv2
-import numpy as np
 import streamlit as st
-from keras.models import model_from_json
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoHTMLAttributes
+import numpy as np
+import av
 
-st.title("Sign Language Detection")
+st.title("OpenCV Filters on Video Stream")
 
-# Load the pre-trained model
-json_file = open("sldnewmodel.json", "r")
-model_json = json_file.read()
-json_file.close()
-model = model_from_json(model_json)
-model.load_weights("sldnewmodel.h5")
-
-class VideoProcessor(VideoTransformerBase):
-    def __init__(self):
-        self.label = ['HI','I LOVE YOU','NO','THUMPS UP','YES', 'blank']
-
-    def transform(self, frame):
-        try:
-            img = frame.to_ndarray(format="bgr24")
-            # Preprocessing steps
-            cropframe = img[40:300, 0:300]
-            cropframe = cv2.cvtColor(cropframe, cv2.COLOR_BGR2GRAY)
-            cropframe = cv2.resize(cropframe, (48, 48))
-            cropframe = cropframe.reshape(1, 48, 48, 1) / 255.0
-
-            # Make prediction
-            pred = model.predict(cropframe)
-            prediction_label = self.label[np.argmax(pred)]
-            accuracy = np.max(pred) * 100
-
-            # Overlay text on the frame
-            if prediction_label != 'blank':
-                cv2.putText(img, f'{prediction_label}   {accuracy:.2f}%', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-            return img
-        except Exception as e:
-            print(f"Exception at transform func = ", e)
+filter = "none"
 
 
-webrtc_ctx = webrtc_streamer(
-    key="example",
-    video_processor_factory=VideoProcessor,
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
+def transform(frame: av.VideoFrame):
+    img = frame.to_ndarray(format="bgr24")
+
+    if filter == "blur":
+        img = cv2.GaussianBlur(img, (21, 21), 0)
+    elif filter == "canny":
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+    elif filter == "grayscale":
+        # We convert the image twice because the first conversion returns a 2D array.
+        # the second conversion turns it back to a 3D array.
+        img = cv2.cvtColor(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
+    elif filter == "sepia":
+        kernel = np.array(
+            [[0.272, 0.534, 0.131], [0.349, 0.686, 0.168], [0.393, 0.769, 0.189]]
+        )
+        img = cv2.transform(img, kernel)
+    elif filter == "invert":
+        img = cv2.bitwise_not(img)
+    elif filter == "none":
+        pass
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
+
+with col1:
+    if st.button("None"):
+        filter = "none"
+with col2:
+    if st.button("Blur"):
+        filter = "blur"
+with col3:
+    if st.button("Grayscale"):
+        filter = "grayscale"
+with col4:
+    if st.button("Sepia"):
+        filter = "sepia"
+with col5:
+    if st.button("Canny"):
+        filter = "canny"
+with col6:
+    if st.button("Invert"):
+        filter = "invert"
+
+
+webrtc_streamer(
+    key="streamer",
+    video_frame_callback=transform,
+    sendback_audio=False
+    )
